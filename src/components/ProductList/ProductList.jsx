8b83a6549b954dc4fc17626453c843073120a3a4
staroverovmv <staroverovmv@data-frame.ru>
2023-07-12 01:04:12 +0300

    8

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -22,7 +22,7 @@ const getTotalPrice = (items = []) => {
 
 const ProductList = () => {
   const [addedItems, setAddedItems] = useState([]);
-  const {tg} = useTelegram;
+  const {tg} = useTelegram();
   const onAdd = (product) => {
     const alreadyAdded = addedItems.find(item => item.id === product.id);
         let newItems = [];
