b8e8ea1019c82359fa381708fc10a33743784500
staroverovmv <staroverovmv@data-frame.ru>
2023-07-12 00:55:29 +0300

    8

--- src/components/ProductList/ProductList.jsx
+++ src/components/ProductList/ProductList.jsx
@@ -46,7 +46,7 @@ const ProductList = () => {
   }
   return (
     <div className={'list'}>
-      {ProductList.map(item => (
+      {products.map(item => (
         <ProductItem
           product={item}
           onAdd={onAdd}
